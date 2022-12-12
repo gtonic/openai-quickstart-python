42ec4c5798d1d1c3a66ec7b44f7157e252a40cdb
Thomas Geiger <tom.geiger@alp54.com>
2022-12-12 17:03:49 +0100

    product name generator

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        product = request.form["product"]
+        feature = request.form["feature"]
         response = openai.Completion.create(
             model="text-davinci-002",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(product, feature),
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
@@ -22,14 +23,17 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_prompt(product, feature):
+    return """Suggest three names for a product that has the following feature. Blend the result with a well known movie character.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
+Product: Container Orchestration
+Feature: Scale well, resilient
+Names: Happy Helios, Mighty manager, Solid Vessel
+Product: Core Banking
+Feature: Monolith, complicated system, proprietary  
+Names: Mighty abyss, Transactional booker, Precise Moneystracker
+Product: {}
+Feature: {}
 Names:""".format(
-        animal.capitalize()
+        product.capitalize(), feature.capitalize()
     )
